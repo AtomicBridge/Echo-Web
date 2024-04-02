40ba31e6686a4a13512cb7d7a3b40cdb086a4dd2
ShiekhAmish <77582706+ShiekhAmish@users.noreply.github.com>
2024-04-02 17:46:41 +0500

    I sett the Authentication image in the project
    I sett the Authentication image in the project

--- src/Components/User/AuthenticateImage.jsx
+++ src/Components/User/AuthenticateImage.jsx
@@ -6,7 +6,7 @@ const AuthenticateImage = () => {
 
 <div className="col-md-6 px-0 d-none d-md-block position-relative">
   <img src="./img/user/login/authenticatebackgroound.png"
-    alt="Login image" className="w-100 vh-100" style={{ objectFit: 'cover', objectPosition: 'left' }} />
+    alt="Login image" className="w-100 " style={{ objectFit: 'cover', objectPosition: 'left', height:'100vh', minHeight:'100%' }} />
   <div className="position-absolute bottom-0 start-50 translate-middle-x  text-white mb-5">
     <h3 className="fw-bold my-3">Welcome to Your Personalized Fitness Journey!</h3>
     <p className='mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius vehicula malesuada. Proin vehicula erat massa, vitae dictum ex bibendum ut. Aenean aliquam hendrerit dui a varius.</p>
